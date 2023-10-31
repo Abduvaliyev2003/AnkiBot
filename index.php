1fdfd106a6e4011995847aa94925d80e6820e1fe
Abduvaliyev2003 <103238177+Abduvaliyev2003@users.noreply.github.com>
2023-10-31 15:34:17 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require_once './Telegram.php';
 require_once './Model/User.php';
 // Replace 'YOUR_API_TOKEN' with the actual API token provided by BotFather
-$apiToken = '6959983012:AAFXxr0y2HB2AcutbHFgVy7aSacZq73P4Nw';
+$apiToken = "';
 
-$telegram = new Telegram('6959983012:AAFXxr0y2HB2AcutbHFgVy7aSacZq73P4Nw');
+$telegram = new Telegram($apiToken);
 
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
